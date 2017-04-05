fe66fe96a4b9c7a2b47905d6dc37cf874c8a24f5
Dylienne <dylienneevery@192.168.2.10>
2017-04-05 23:02:38 +0200

    started script for extraction
    For prov documents with more than 3.5K lines

---
+++ JSON.py
@@ -0,0 +1,44 @@
+import json
+import numpy as np
+
+#PROV file from PROV store with JSON conversion
+file= read.json(...)
+
+# create file with all prov:entities from W3 websites
+
+#read document
+def readDocument():
+    open "path" as JSONPYTHON.readlines()
+
+#identify all entities regarding prov:x in JSON
+def extractentity():
+    number = []
+    for x in file:
+        if line[0:10]== "prov:X"
+            number.append(line)
+            return number
+
+#count entities types in repositories
+def countentities()
+    counter = 0
+    for number in extractentity():
+        if number == number:
+            print()
+
+
+def calculateInstances():
+    amountofinstances= [list]
+
+def histogram():
+    x= countentities().counter
+    y= len(calculateInstances())
+        np.histogram2d(x,y)
+
+#percentage and piecharts in entity distributions
+
+#program
+
+readDocument()
+extractentity()
+
+np.histogram2d()
